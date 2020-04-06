4bb5d4600b1ddd43d6e637c680fb9ed0d92cf8fb
223131 <60364232+223131@users.noreply.github.com>
2020-04-06 11:26:06 -0500

    Create FinalJS.js

---
+++ Version9.0/user03/JS/FinalJS.js
@@ -0,0 +1,13 @@
+function ChildrensMuseum() {
+  alert("Silly human being, this page is for kids!");
+}
+
+function bigImg(x) {
+  x.style.height = "64px";
+  x.style.width = "64px";
+}
+
+function normalImg(x) {
+  x.style.height = "50px";
+  x.style.width = "50px";
+}
