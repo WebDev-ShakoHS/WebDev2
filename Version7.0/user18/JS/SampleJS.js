fed70b801f9c2599ed33699b98bb2debba32bd23
NoahPeterson <73717263+NoahPeterson@users.noreply.github.com>
2021-05-20 14:25:45 -0500

    Update

--- Version7.0/user18/JS/SampleJS.js
+++ Version7.0/user18/JS/SampleJS.js
@@ -1,3 +1,36 @@
 function scrollWindow() {
-    window.scrollTo(0,0)
+    window.scrollTo(0, 0)
+}
+
+$("#myCarousel").carousel();
+
+$(".item").click(function () {
+    $("#myCarousel").carousel(1);
+});
+
+$(".left").click(function () {
+    $("#myCarousel").carousel("prev");
+});
+
+function openNav() {
+    document.getElementById("mySidebar").style.width = "250px";
+    document.getElementById("main").style.marginLeft = "250px";
+  }
+  
+  function closeNav() {
+    document.getElementById("mySidebar").style.width = "0";
+    document.getElementById("main").style.marginLeft= "0";
+  }
+
+function move2019() {
+    window.location.href = "2018-2021.html";
+}
+function move2009() {
+    window.location.href = "2009-2013.html";
+}
+function move2014() {
+    window.location.href = "2014-2017.html";
+}
+function moveHome() {
+    window.location.href = "Home.html"
 }
