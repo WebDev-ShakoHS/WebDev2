5c2a352120cb40996e7f1fa5ba2cb72563d6d5e9
Bhavesh-B21 <234793@shakopeeschools.org>
2021-05-23 17:39:12 -0500

    1.7 website
    added alert and click js functions

---
+++ Version7.0/user04/JS/finalscript.js
@@ -0,0 +1,7 @@
+function color(elmnt, clr) {
+    elmnt.style.color = clr;
+}
+
+function myFunction() {
+    alert("Trade at your own risk. All recommendations are our own. Trading is very risky and NO guarantees can be made. Past performance is not indicative of future results.");
+}
