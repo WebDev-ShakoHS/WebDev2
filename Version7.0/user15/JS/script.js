d5c1cb2cff64e8731c4fb146e7e65b7ffe96bb24
maxmeeh <82399362+maxmeeh@users.noreply.github.com>
2021-05-24 23:29:46 -0500

    Finished 1.7

---
+++ Version7.0/user15/JS/script.js
@@ -0,0 +1,44 @@
+
+
+function confirmInput() {
+  fname =
+    document.forms[0].fname.value;
+  alert("Hello you have succesfully logged in");
+}
+
+function sports() {
+  document.getElementById("demo").innerHTML = "Probably the best past time";
+}
+
+function traveling() {
+  document.getElementById("gaming").innerHTML = "you chose the wrong one. Tooooo Expensive!!!!!!!";
+}
+
+function gaming() {
+  document.getElementById("demo").innerHTML = "Cant go wrong with gaming";
+}
+
+
+
+function preferedBrowser() {
+  prefer = document.forms[0].browsers.value;
+  alert("browsers");
+}
+
+
+
+function preferedBrowser() {
+  alert("Packers are my favorite but as long as you dont pick the vikings everythings fine.");
+}
+
+src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"
+src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"
+
+
+function mouseDown() {
+  document.getElementById("myP").style.color = "red";
+}
+
+function mouseUp() {
+  document.getElementById("myP").style.color = "green";
+}
