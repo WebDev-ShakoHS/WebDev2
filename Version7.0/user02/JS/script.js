4e1e0de2c17e9843375c4d87e5032acbc330aacb
Akshay-bod <230289@shakopeeschools.org>
2021-05-25 13:15:06 -0500

    Create script.js

---
+++ Version7.0/user02/JS/script.js
@@ -0,0 +1,37 @@
+function buttonChange1(x) {
+    x.style.background = "yellow";
+}
+
+function textChange1() {
+    document.getElementById("b1").innerHTML = "Purchased!"
+}
+
+function buttonChange2(x) {
+    x.style.background = "yellow";
+}
+
+function textChange2() {
+    document.getElementById("b2").innerHTML = "Purchased!"
+}
+
+function buttonChange3(x) {
+    x.style.background = "yellow";
+}
+
+function textChange3() {
+    document.getElementById("b3").innerHTML = "Purchased!"
+}
+
+function buttonChange4(x) {
+    x.style.background = "yellow";
+}
+
+function textChange4() {
+    document.getElementById("b4").innerHTML = "Purchased!"
+}
+
+function support(x) {
+    document.getElementById("submitButton").innerHTML = "Submitted your report!"
+    x.style.background = "green";
+    document.getElementById("input1").innerHTML = " "
+}
