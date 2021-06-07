607f6b9c71cf66a2aecd780ae7c04e8b849c1da6
Bhavesh-B21 <234793@shakopeeschools.org>
2021-06-07 13:53:27 -0500

    final
    api call and data

--- Version9.0/user04/stockapi.php
+++ Version9.0/user04/stockapi.php
@@ -19,12 +19,60 @@ curl_setopt_array($curl, [
 
 $response = curl_exec($curl);
 $err = curl_error($curl);
+
 curl_close($curl);
 
 $data = json_decode($response,true);
 
 echo $data[0]['symbol'];
 echo $data[0]['ask'];
+echo $data[1]['symbol'];
+echo $data[1]['ask'];
+echo $data[2]['symbol'];
+echo $data[2]['ask'];
+echo $data[3]['symbol'];
+echo $data[3]['ask'];
+echo $data[4]['symbol'];
+echo $data[4]['ask'];
+echo $data[5]['symbol'];
+echo $data[5]['ask'];
+echo $response;
 
 ?>
+
+<!doctype html>
+<html>
+<head>
+<title>Apple and Microsoft quotes</title>
+
+<style>
+
+
+
+.price {
+    line-height: 25px;
+}
+</style>    
+
+
+</head>
+<body>
+
+    <div class="report-container">
+        <h2><?php echo $data->name; ?>Quotes</h2>
+        <div class="price">
+            <div><Span>Apple: </span><?php echo $data[0]['ask'] ; ?></div>
+            <div><Span>Microsoft: </span> <?php echo $data[1]['ask'] ; ?></div>
+            <div><Span>Facebook: </span> <?php echo $data[2]['ask'] ; ?></div>
+            <div><Span>Netflix: </span><?php echo $data[3]['ask'] ; ?></div>
+            <div><Span>Amazon: </span><?php echo $data[4]['ask'] ; ?></div>
+            <div><Span>Google: </span><?php echo $data[5]['ask'] ; ?></div>
+        </div>
+    </div>
+
+
+</body>
+</html>
+
+
 
